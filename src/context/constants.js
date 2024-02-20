9e60f6a697e97c47bf0b988bd20d6e2866fea2bb
Jorge Oehrens <jorge.oehrens@gmail.com>
2024-02-20 10:46:01 -0300

    Checkout1

---
+++ src/context/constants.js
@@ -0,0 +1,13 @@
+//TheBlockchainCoders = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+//TokenSale: = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+
+import TheBlockchainCoders from "./TheBlockchainCoders.json";
+import TokenSale from "./TokenSale.json";
+
+//TOKEN
+export const TOKEN_ADDRESS = "0x36e042cfde642c3e1e39ee4ea40161cce68cafab";
+export const TOKEN_ABI = TheBlockchainCoders.abi;
+
+//TOKEN SALE
+export const TOKEN_SALE_ADDRESS = "0xcce8d3d982f77e0a347980ea56f193bfde576d4b";
+export const TOKEN_SALE_ABI = TokenSale.abi;
